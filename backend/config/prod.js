55c81d655cc92e99faae38246159576c7681fcf7
sourabh <kanadesourabh420@gmail.com>
2023-03-21 11:12:15 +0530

    imp files restricted

--- backend/config/prod.js
+++ backend/config/prod.js
@@ -1,10 +1,5 @@
 module.exports = {
-  
 JWT_SEC: process.env.JWT_SEC,
 MONGO_URL : process.env.MONGO_URL,
 PASS_SEC : process.env.PASS_SEC,
 }
-
-// MONGO_URL : 'mongodb+srv://SourabhKanade:8888181326@reactapp1.9prdi.mongodb.net/shop?retryWrites=true&w=majority',
-// PASS_SEC : 'SKANADE',
-// JWT_SEC : 'DEVELOPER42',
